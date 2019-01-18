4e7ca912ba27c11c03ae05f5bde5f369dabad462
ezio <ezioreturner@gmail.com>
2019-01-18 14:01:10 +0800

    1

--- src/app/content.js
+++ src/app/content.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react'
-import {Card} from 'antd';
+import {Card, Row, Col} from 'antd';
 import 'style/content.scss';
+
+class CardTitle extends Component {
+    render() {
+        const {title} = this.props;
+        return (<div className="titleNanme">{title}</div>)
+    }
+}
+
+class EchartCard extends Component {
+    render() {
+        const headStyle = {
+            borderBottomColor: '#f5f5f5'
+        };
+        const {title} = this.props;
+        return (<Card size="small" title={<CardTitle title = {
+                title
+            } />} headStyle={headStyle} bordered={false}></Card>)
+    }
+}
+
+class RowDom extends Component {
+    render() {
+        return (<Row gutter={16}>
+            <Col className="gutter-row" span={12}>
+                <EchartCard title="性别"/>
+            </Col>
+            <Col className="gutter-row" span={12}>
+                <EchartCard title="年龄"/>
+            </Col>
+        </Row>)
+    }
+}
+
 class Content extends Component {
     render() {
         return (<div className="content">
-            <Card title="card"></Card>
-            <Card title="card" style={{
-                    marginTop: '23px'
-                }}></Card>
+            <RowDom/>
         </div>)
     }
 }
