491b2584717fa5e80d0d58f31b07226202553e0b
zhev <ezioreturner@gmail.com>
2019-01-19 15:09:29 +0800

    add event

--- src/app/header/index.js
+++ src/app/header/index.js
@@ -1,19 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'style/header.scss';
-import {Icon} from 'antd';
+import { Icon } from 'antd';
 import Clock from './clock';
 class Header extends Component {
     constructor(props) {
         super(props);
     }
+    handleClick(query, e) {
+        // console.log(this, query, e)
+    }
     render() {
         return (<div className="header">
             <div className="controlBut">
-                <Icon type="bars"/>
+                <Icon type="bars" />
             </div>
-            <span className="title">
+            <span className="title" onClick={this.handleClick.bind(this, 'hahah')}>
                 REACT-TURBO</span>
-            <Clock/>
+            <Clock />
         </div>)
     }
 }
