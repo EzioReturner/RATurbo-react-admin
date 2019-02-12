f546c8c6ccce642e458e3df8182d18bc0440e1f2
zhev <ezioreturner@gmail.com>
2019-02-12 22:48:01 +0800

    2

---
+++ src/pages/Dashboard/Controller.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Card, Select, Button, DatePicker, Icon, Switch } from 'antd';
+const { Option } = Select;
+
+class Controller extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	getOptions() {
+		return Array(5)
+			.fill(1)
+			.map((res, index) => {
+				return (
+					<Option key={index}>
+						{Math.random()
+							.toString(36)
+							.substr(2)}
+					</Option>
+				);
+			});
+	}
+
+	render() {
+		const marginRight = {
+			sx: {
+				marginRight: '16px'
+			},
+			et: {
+				marginRight: '8px'
+			}
+		};
+
+		const ControlHead = (
+			<div className="headerContent">
+				<Icon type="filter" />
+				<span className="headerTextSpan">筛选条件</span>
+				<Select placeholder="全部APP" size="small">
+					{this.getOptions()}
+				</Select>
+				<Select placeholder="双平台" size="small">
+					{this.getOptions()}
+				</Select>
+				<DatePicker size="small" style={marginRight.sx} />
+				<Button type="primary" size="small">
+					查询
+				</Button>
+			</div>
+		);
+
+		return (
+			<Card
+				size="small"
+				title={ControlHead}
+				bordered={false}
+				hoverable
+				className="controlHead"
+				bodyStyle={{
+					display: 'flex',
+					alignItems: 'center'
+				}}
+			>
+				<Switch defaultChecked={true} style={marginRight.et} />
+				<span className="headerTextSpan">未识别</span>
+				<span className="headerTextSpan rightSpace">
+					符合当前筛选条件的总用户数为：21,312,393
+					<Icon type="question-circle" />
+				</span>
+			</Card>
+		);
+	}
+}
+
+export default Controller;
---
+++ src/pages/Dashboard/chartOption.js
@@ -0,0 +1,231 @@
+const circleOption = {
+  legend: {
+    orient: 'vertical',
+    right: 20,
+    data: [
+      {
+        name: '男',
+        icon: 'circle'
+      }, {
+        name: '女',
+        icon: 'circle'
+      }, {
+        name: '未识别',
+        icon: 'circle'
+      }
+
+    ]
+  },
+  series: [
+    {
+      name: '访问来源',
+      type: 'pie',
+      radius: [
+        '40%', '75%'
+      ],
+      center: [
+        '45%', '50%'
+      ],
+      avoidLabelOverlap: false,
+      label: {
+        normal: {
+          show: true,
+          position: 'outside',
+          formatter: '{d}%',
+          fontWeight: 'bold'
+        },
+        emphasis: {
+          show: true,
+          textStyle: {
+            fontSize: '30',
+            fontWeight: 'bold'
+          }
+        }
+      },
+      labelLine: {
+        normal: {
+          show: false
+        }
+      },
+      data: [
+        {
+          value: 335,
+          name: '男',
+          itemStyle: {
+            color: 'rgba(251, 68, 145, 0.8)'
+          }
+        }, {
+          value: 310,
+          name: '女',
+          itemStyle: {
+            color: 'rgba(220, 91, 231, 0.8)'
+          }
+        }, {
+          value: 234,
+          name: '未识别',
+          itemStyle: {
+            color: '#d9d9d9'
+          }
+        }
+      ]
+    }
+  ]
+};
+
+const barOption = {
+  grid: {
+    left: 10,
+    right: 10,
+    top: 40,
+    bottom: 40
+  },
+  xAxis: {
+    type: 'category',
+    data: [
+      '未识别',
+      '0-17',
+      '18-24',
+      '25-34',
+      '35-44',
+      '45+'
+    ],
+    axisLabel: {
+      color: '#c9c9c9'
+    },
+    axisLine: {
+      show: false
+    },
+    axisTick: {
+      show: false
+    }
+  },
+  yAxis: {
+    type: 'value',
+    axisLine: {
+      show: false
+    },
+    axisTick: {
+      show: false
+    },
+    axisLabel: {
+      show: false
+    },
+    splitLine: {
+      lineStyle: {
+        color: '#f6f6f6'
+      }
+    }
+  },
+  series: [
+    {
+      barCategoryGap: '50%',
+      data: [
+        {
+          value: 45,
+          itemStyle: {
+            color: '#d9d9d9'
+          }
+        },
+        4.2,
+        22.5,
+        22.30,
+        '8.00',
+        2.53
+      ],
+      type: 'bar',
+      label: {
+        show: true,
+        position: 'top',
+        color: '#c9c9c9',
+        formatter: '{c}%'
+      },
+      itemStyle: {
+        color: 'rgba(251, 68, 145, 0.8)'
+      },
+      z: 2
+    }, {
+      type: 'bar',
+      silent: true,
+      data: [
+        45,
+        45,
+        45,
+        45,
+        45,
+        45
+      ],
+      barGap: '-100%',
+      itemStyle: {
+        color: '#f9f9f9'
+      },
+      z: 1
+    }
+  ]
+};
+
+const cityOption = {
+  xAxis: {
+    data: [
+      '未识别',
+      '上海',
+      '北京',
+      '深圳',
+      '广州',
+      '杭州'
+    ]
+  },
+  series: [
+    {
+      data: [
+        {
+          value: 45,
+          itemStyle: {
+            color: '#d9d9d9'
+          }
+        },
+        14.2,
+        2.5,
+        7.30,
+        '19.00',
+        33.53
+      ]
+    }
+  ]
+}
+
+const provinceOption = {
+  xAxis: {
+    data: [
+      '未识别',
+      '四川',
+      '福建',
+      '广东',
+      '浙江',
+      '河南'
+    ]
+  },
+  series: [
+    {
+      data: [
+        {
+          value: 45,
+          itemStyle: {
+            color: '#d9d9d9'
+          }
+        },
+        18.2,
+        22.5,
+        33.30,
+        '9.00',
+        13.53
+      ]
+    }
+  ]
+}
+
+export {
+  circleOption,
+  barOption,
+  cityOption,
+  provinceOption
+}
