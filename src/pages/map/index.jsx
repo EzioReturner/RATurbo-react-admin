9da7eb1a9f018637f58267dce83e41eac31f7c83
ezio <ezioreturner@gmail.com>
2019-02-13 09:23:31 +0800

    chang map

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,16 +1,29 @@
-import React, { Component } from "react";
-import "./myMap.scss";
-import BMap from "BMap";
+import React, { Component } from 'react';
+import './myMap.scss';
+import BMap from 'BMap';
+import { Card } from 'antd';
 class Map extends Component {
 	componentDidMount() {
-		const map = new BMap.Map("mapContainer");
+		const map = new BMap.Map('mapContainer');
 		map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
-		map.setCurrentCity("北京");
+		map.setCurrentCity('北京');
 		map.enableScrollWheelZoom(true);
+		map.setMapStyle({ style: 'midnight' });
 	}
 
 	render() {
-		return <div className="myMapContainer" id="mapContainer" />;
+		return (
+			<Card
+				title="地图"
+				bodyStyle={{
+					height: 'calc(100% - 48px)',
+					width: '100%',
+					padding: '24px'
+				}}
+			>
+				<div className="myMapContainer" id="mapContainer" />
+			</Card>
+		);
 	}
 }
 
