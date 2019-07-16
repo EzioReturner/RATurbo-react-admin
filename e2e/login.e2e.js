1037a0da2b83623fc12fc86c3b8d3fc7e7412b9b
ezio <ezioreturner@gmail.com>
2019-07-16 17:00:49 +0800

    1

--- e2e/login.e2e.js
+++ e2e/login.e2e.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const BASE_URL = `http://localhost:${process.env.PORT || 9527}`;
 
-describe('logind', () => {
+describe('login', () => {
   let browser;
   let page;
 
@@ -22,11 +22,11 @@ describe('logind', () => {
     await page.waitFor(selector => !!document.querySelector('#login_userName'), {
       timeout: 3000,
     });
-    await page.type('#login_userName', 'wrong_user')
-    await page.type('#login_password', 'wrong_password')
-    await page.click('button[type="submit"]')
-    await page.waitForSelector('.anticon-close-circle') // should display error
-  })
+    await page.type('#login_userName', 'wrong_user');
+    await page.type('#login_password', 'wrong_password');
+    await page.click('button[type="submit"]');
+    await page.waitForSelector('.anticon-close-circle'); // should display error
+  });
 
-  afterAll(() => browser.close())
-})
+  afterAll(() => browser.close());
+});
