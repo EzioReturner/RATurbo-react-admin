fa3d5589e787cb5b5c9e663d7bd8c2cac9f3e26a
ezio <ezioreturner@gmail.com>
2019-08-31 15:12:12 +0800

    empty

--- client/views/Form/Test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test = props => {
-  return <div>{props.children}</div>;
-};
-
-export default Test;
--- client/views/Form/TestDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TestDetail = () => {
-  return <div>testDetail</div>;
-};
-
-export default TestDetail;
