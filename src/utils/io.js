e2d13cc5362f5e25eb40013117649be46d892ec3
ezio <ezioreturner@gmail.com>
2019-09-07 11:32:38 +0800

    update io

--- src/utils/io.js
+++ src/utils/io.js
@@ -1,83 +1,117 @@
-import axios from 'axios';
-import { notification } from 'antd';
+import Axios from 'axios';
+import { Notification } from 'element-ui';
 
 class Request {
   instance;
 
   constructor() {
-    this.instance = axios.create();
-    this.initTnterceptors();
+    this.instance = Axios.create({
+      withCredentials: true
+    });
+    this.initInterceptors();
   }
 
-  // 初始化拦截器
-  initTnterceptors() {
+  // 拦截器
+  initInterceptors() {
     this.instance.interceptors.request.use(
       config => {
         return config;
       },
       error => {
-        Promise.reject(error);
+        return Promise.reject(error);
+      }
+    );
+    this.instance.interceptors.response.use(
+      response => {
+        return response;
+      },
+      error => {
+        return Promise.reject(error.response);
       }
     );
   }
 
-  // 设置自定义头部
   setHeader = (key, val) => {
     this.instance.defaults.headers.common[key] = val;
   };
 
-  // 错误notify
   notify(message) {
-    notification.error({
-      message: '请求错误',
-      description: `${message ||
-        'This is the content of the notification. This is the content of the notification. This is the content of the notification.'}`
+    Notification({
+      type: 'error',
+      title: '接口异常',
+      message
     });
   }
 
-  // 错误处理
   handleError = error => {
-    const { message, status } = error;
+    const {
+      data: { message, name, msg },
+      status
+    } = error;
+    if (message === 'SUCCESS' && status === 0) {
+      return;
+    }
     switch (status) {
       case 401:
         break;
       case 404:
+        this.notify('接口地址 404， 请检查接口地址确认接口已发布' || error);
         break;
       case 500:
+        this.notify(name || error);
         break;
       default:
-        this.notify(message || error);
+        this.notify(message || msg || error);
         break;
     }
     return Promise.reject(error);
   };
 
-  sendRequest(method, data) {
-    let { path, params, options } = data;
-    const _query = options ? { ...options, params } : { params };
-    return this.instance[method](path, _query).catch(this.handleError);
+  filterStatus = data => {
+    const {
+      data: { status }
+    } = data;
+    if (status !== 0) {
+      return Promise.reject(data);
+    }
+    return data;
+  };
+
+  sendRequest(method, path, data = {}) {
+    let { params, options = {} } = data;
+    return this.instance[method](path, params, options)
+      .then(this.filterStatus)
+      .catch(this.handleError);
   }
 
-  get(path, data) {
-    return this.sendRequest('get', { path, ...data });
+  get(path, data = {}) {
+    const { params } = data;
+    let _path = path;
+    if (params) {
+      _path += '?';
+      Object.keys(params).forEach(key => {
+        _path += `${key}=${params[key]}&`;
+      });
+      _path = _path.replace(/&$/, '');
+    }
+    return this.sendRequest('get', _path, data);
   }
 
   post(path, data) {
-    return this.sendRequest('post', { path, ...data });
+    return this.sendRequest('post', path, data);
   }
 
   put(path, data) {
-    return this.sendRequest('put', { path, ...data });
+    return this.sendRequest('put', path, data);
   }
 
   patch(path, data) {
-    return this.sendRequest('patch', { path, ...data });
+    return this.sendRequest('patch', path, data);
   }
 
   delete(path, data) {
-    return this.sendRequest('delete', { path, ...data });
+    return this.sendRequest('delete', path, data);
   }
 }
-const request = new Request();
 
-export default request;
+export default new Request();
