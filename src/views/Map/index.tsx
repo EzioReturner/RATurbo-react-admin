d1f84eebedb9e5df76c0a64cfbc828218ef71ed7
ezio <ezioreturner@gmail.com>
2019-10-04 13:14:33 +0800

    update map view

--- src/views/Map/index.tsx
+++ src/views/Map/index.tsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card } from 'antd';
+// @ts-ignore
 import BMap from 'BMap';
 import './myMap.scss';
 
-class Map extends Component {
+class Map extends React.Component {
   componentDidMount() {
     const map = new BMap.Map('mapContainer');
     map.centerAndZoom(new BMap.Point(108.154, 37.495), 5);
