bc92c7ea0353d11f77ddab98cd0742a52ecf8386
ezio <ezioreturner@gmail.com>
2019-10-12 11:30:41 +0800

    update unit test

--- e2e/login.e2e.js
+++ e2e/login.e2e.js
@@ -12,7 +12,7 @@ describe('login', () => {
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto(`${BASE_URL}/#/user/login`, {
-      waitUntil: 'networkidle2',
+      waitUntil: 'networkidle2'
     });
   });
 
@@ -20,12 +20,22 @@ describe('login', () => {
 
   it('should login with failure', async () => {
     await page.waitFor(selector => !!document.querySelector('#login_userName'), {
-      timeout: 3000,
+      timeout: 3000
     });
     await page.type('#login_userName', 'wrong_user');
     await page.type('#login_password', 'wrong_password');
     await page.click('button[type="submit"]');
-    await page.waitForSelector('.anticon-close-circle'); // should display error
+    await page.waitForSelector('.anticon-close-circle');
+  });
+
+  it('should login with success', async () => {
+    await page.waitFor(selector => !!document.querySelector('#login_userName'), {
+      timeout: 3000
+    });
+    await page.type('#login_userName', 'admin');
+    await page.type('#login_password', '123');
+    await page.click('button[type="submit"]');
+    await page.waitForSelector('.ant-message-success');
   });
 
   afterAll(() => browser.close());
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -6,8 +6,9 @@ module.exports = {
   //   debug: true,
   // },
   launch: {
-    headless: true, //设定运行模式，false的情况下将会工作在有GUI界面的模式，true则不开启GUI界面
+    headless: true //设定运行模式，false的情况下将会工作在有GUI界面的模式，true则不开启GUI界面
     //设定本地Chrome路径，官方推荐使用Chrome Canary
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   }
 };
+// 默认会跳过安装chromium 如果需要安装chromium请运行此命令 node node_modules/puppeteer/install.js
