a5d154a4b05e37bf5422faeb4e6f8104bb2b8159
ezio <ezioreturner@gmail.com>
2020-02-11 14:33:50 +0800

    增加高亮组件

---
+++ src/components/HighLight/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, Fragment } from 'react';
+
+interface HighLightProps {
+  val: string;
+  tarVal: string;
+}
+
+const HighLight: React.FC<HighLightProps> = props => {
+  const [unmatchVal, setUnmatchVal] = useState<string[]>([]);
+  const { val, tarVal } = props;
+
+  const checkText = (_val: string, cacheUnmatch: string[]) => {
+    const index = _val.indexOf(tarVal);
+
+    if (index < 0 || !tarVal) {
+      setUnmatchVal([...cacheUnmatch, _val]);
+      return;
+    }
+    const queryScope = index + tarVal.length;
+    const head = _val.slice(0, index);
+    const surplus = _val.substr(queryScope);
+    cacheUnmatch.push(head);
+    checkText(surplus, cacheUnmatch);
+  };
+
+  useEffect(() => {
+    let cacheUnmatch: string[] = [];
+    checkText(val, cacheUnmatch);
+  }, [tarVal]);
+
+  return (
+    <>
+      {unmatchVal.length ? (
+        unmatchVal.map((text, index) => {
+          return (
+            <Fragment key={index}>
+              <span>{text}</span>
+              {index !== unmatchVal.length - 1 ? (
+                <span style={{ color: '#1e90ff' }}>{tarVal}</span>
+              ) : null}
+            </Fragment>
+          );
+        })
+      ) : (
+        <span>{val}</span>
+      )}
+    </>
+  );
+};
+
+export default HighLight;
--- src/config/router.config.ts
+++ src/config/router.config.ts
@@ -19,16 +19,6 @@ export const constantRouteConfig: { app: RouteConfig; user: RouteConfig } = {
   }
 };
 
-/**
- * 路由配置
- * @param path {string} 路由路径
- * @param name {string} 菜单名称、可以作为key来映射国际化菜单名
- * @param icon {string} 菜单图标
- * @param componet {string[]} 加载组件的项目路径位置，必须为数组，第一项路径，第二项配置组件进入动画class
- * @param hiddeMenu {boolean} 菜单中是否显示
- * @param routes {object[]} 子路由信息
- * @param rest 可接收 react-router-dom 参数
- */
 export const asyncRouteConfig: RouteChild[] = [
   {
     name: 'dashboard',
