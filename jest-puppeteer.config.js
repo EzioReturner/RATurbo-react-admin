cc6f49a8a15a55be9a8ae0a41cacc25cba5b3661
ezio <ezioreturner@gmail.com>
2020-03-28 10:45:03 +0800

    增加jest svg支持

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   launch: {
     args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-first-run', '--no-zygote'],
-    headless: false
-    // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    headless: false,
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   }
 };
 // 默认会跳过安装chromium 如果需要安装chromium请运行此命令 node node_modules/puppeteer/install.js
--- src/e2e/jest.config.js
+++ src/e2e/jest.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+
 module.exports = {
-  testRegex: './*\\.e2e\\.js$',
-  preset: 'jest-puppeteer'
+  rootDir: path.resolve(__dirname, '../../'),
+  testRegex: '.mainLayout/*\\.e2e\\.js$',
+  preset: 'jest-puppeteer',
+  moduleNameMapper: {
+    // 主要用于与webpack的resolve.alias匹配，注意正则写法
+    '^@assets(.*)$': '<rootDir>/src/assets$1',
+    '^@components(.*)$': '<rootDir>/src/components$1'
+  },
+  transform: {
+    '^.+\\.(tsx|js|ts)?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-svg-transformer'
+  }
 };
