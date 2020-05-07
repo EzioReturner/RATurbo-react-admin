8eb12090df9d7b65ac06b2acc9b62d52ef2d6054
ezio <ezioreturner@gmail.com>
2020-05-07 18:00:25 +0800

    移除 dynamic-antd-theme，迁移部分颜色

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -118,7 +118,7 @@ module.exports = {
       antDir: _resolve('./node_modules/antd'),
       stylesDir: _resolve('./src/styles'),
       varFile: _resolve('./src/styles/variables.less'),
-      mainLessFile: _resolve('./src/styles/entry.less'),
+      mainLessFile: _resolve('./src/styles/lessEntry.less'),
       outputFilePath: _resolve('./public/color.less'),
       themeVariables: ['@primary-color'],
       indexFileName: './public/index.html',
