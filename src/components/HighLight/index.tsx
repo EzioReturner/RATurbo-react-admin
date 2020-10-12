2dd09720b8f92a8f3f0e6e57c5f982107676a4db
ezio <ezioreturner@gmail.com>
2020-10-12 16:08:30 +0800

    更新高亮组件

--- src/components/HighLight/index.tsx
+++ src/components/HighLight/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, Fragment, memo, useCallback } from 'react';
+
 interface HighLightProps {
   val: string; // 原始字段
   tarVal: string; // 需要匹配高亮的字段
+  matchAll?: boolean; // 全局匹配模式  目标数据为 111 tar 为 1 时则全部高亮
 }
 
 const isEqual = (prevProps: HighLightProps, nextProps: HighLightProps) => {
@@ -13,25 +15,38 @@ const isEqual = (prevProps: HighLightProps, nextProps: HighLightProps) => {
 
 const HighLight: React.FC<HighLightProps> = props => {
   const [unmatchVal, setUnmatchVal] = useState<string[]>([]);
-  const { val, tarVal } = props;
+  const { val, tarVal, matchAll } = props;
 
   const checkText = useCallback(
     (_val: string, cacheUnmatch: string[]) => {
+      // 找到匹配的位置
       const index = _val.indexOf(tarVal);
 
       if (index < 0 || !tarVal) {
         setUnmatchVal([...cacheUnmatch, _val]);
         return;
       }
+
+      // 剩余的字符串长度
       const queryScope = index + tarVal.length;
+
+      // 匹配字符之前的字符串
       const head = _val.slice(0, index);
+
       const surplus = _val.substr(queryScope);
 
-      cacheUnmatch.push(head);
-      checkText(surplus, cacheUnmatch);
+      if (matchAll) {
+        cacheUnmatch.push(head);
+        checkText(surplus, cacheUnmatch);
+      } else if (index >= 0) {
+        setUnmatchVal([head, surplus]);
+        return;
+      }
     },
-    [tarVal]
+    [tarVal, matchAll]
   );
+
+  console.log('unmatchVal', unmatchVal);
 
   useEffect(() => {
     let cacheUnmatch: string[] = [];
