0605da0ece8d700dd86958d9e0458cd60c0e49f5
ezio <ezioreturner@gmail.com>
2020-12-04 21:28:59 +0800

    小优化drawer组件

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import ReactDOM from 'react-dom';
 import './drawer.less';
@@ -25,39 +25,58 @@ const RADrawer: React.FC<RADrawerProps> = props => {
     direction
   } = props;
 
+  const drawerRef = useRef<HTMLDivElement>(null);
+
   let _direction = direction || 'up';
 
-  return ReactDOM.createPortal(
-    <div
-      className={classnames(
-        'RA-drawer',
-        open && 'RA-drawer-opened',
-        `RA-drawer-direction-${_direction}`
-      )}
-      style={{ ...drawerStyle }}
-    >
-      <div
-        className="RA-drawer-mask"
-        style={{ ...maskStyle }}
-        onClick={() => {
-          openChange(false);
-        }}
-      ></div>
-      <div
-        className="RA-drawer-wrapper"
-        style={{ ...wrapperStyle }}
-        onClick={e => {
-          if (wrapperClose && e.target === e.currentTarget) {
-            openChange(false);
-            typeof wrapperClose === 'function' && wrapperClose();
-          }
-        }}
-      >
-        {children}
-      </div>
-    </div>,
-    document.getElementsByTagName('body')[0]
-  );
-};
+  useEffect(() => {
+    setTimeout(() => {
+      if (drawerRef.current) {
+        if (open) {
+          drawerRef.current.className = drawerRef.current.className + ' RA-drawer-opened';
+        } else {
+          drawerRef.current.className = drawerRef.current.className.replace('RA-drawer-opened', '');
+        }
+      }
+    }, 10);
+  }, [open]);
 
+  let portal = null;
+
+  if (drawerRef.current || open) {
+    portal = ReactDOM.createPortal(
+      <div
+        className={classnames(
+          'RA-drawer',
+          // open && 'RA-drawer-opened',
+          `RA-drawer-direction-${_direction}`
+        )}
+        style={{ ...drawerStyle }}
+        ref={drawerRef}
+      >
+        <div
+          className="RA-drawer-mask"
+          style={{ ...maskStyle }}
+          onClick={() => {
+            openChange(false);
+          }}
+        ></div>
+        <div
+          className="RA-drawer-wrapper"
+          style={{ ...wrapperStyle }}
+          onClick={e => {
+            if (wrapperClose && e.target === e.currentTarget) {
+              openChange(false);
+              typeof wrapperClose === 'function' && wrapperClose();
+            }
+          }}
+        >
+          {children}
+        </div>
+      </div>,
+      document.getElementsByTagName('body')[0]
+    );
+  }
+  return portal;
+};
 export default RADrawer;
